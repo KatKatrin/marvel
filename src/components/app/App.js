71c650406014b15155eeeb76710d5c4f1b7455ae
kate <you@example.com>
2022-01-22 23:24:46 +0200

    final version

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,47 +1,44 @@
-import { Component } from "react";
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import decoration from '../../resources/img/vision.png';
+import ComicsPage from "../pages/ComicsPage";
+import MainPage from "../pages/MainPages";
+import Page404 from "../pages/404";
+import SingleComicPage from '../pages/SingleComicPage'
+import SingleForm from "../pages/singleForm";
 
-class App extends Component {
+
+
+
+
+const App = () => {
+ 
+    return (
+      <Router>
+          <div className="app">
+            <AppHeader/>
+              <main>
+                <Routes>
+                 
+                  <Route exact path='/' element={<MainPage/>}/>
+
+                  <Route exact path='/comics' element={<ComicsPage/>}/>
+
+                  <Route exact path='/comics/:comicId' element={<SingleComicPage/>}/>
+
+                  <Route exact path='/characters/:comicId' element={<SingleForm/>}/>
+
+
+                  <Route path='*' element={<Page404/>}/>
+                     
+                </Routes>   
+            </main>
+        </div> 
+      </Router>
+    )
     
-    state = {
-        selectedChar: null
-    }
-
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id,
-           
-        })
-    }
-
-    render() {
-        return (
-            <div className="app">
-                <AppHeader/>
-                <main>
-                    <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                        <ErrorBoundary>
-                        <CharList onCharSelected={this.onCharSelected}/>
-                        </ErrorBoundary>
-                        
-                        <ErrorBoundary>
-                        <CharInfo charId={this.state.selectedChar}/>
-                        </ErrorBoundary>
-                        
-                    </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
-                </main>
-            </div>
-        )
-    }
 }
 
 export default App;
